7d6846c21db12057d346e69a7daf208a9c5da61f
kenvilar <kenvilar@gmail.com>
2023-05-12 14:53:52 +0800

    implement Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
 
 export default function Home() {
   useEffect(() => {
@@ -22,6 +23,8 @@ export default function Home() {
   return (
     <>
       <Header />
+
+      <Hero />
     </>
   );
 }
