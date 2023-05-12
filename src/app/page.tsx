478c9b38c63f9d02fcaae5f8d828cfc55224fbf5
kenvilar <kenvilar@gmail.com>
2023-05-12 13:34:29 +0800

    add darkModeSwitcher function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+"use client";
+import { useEffect } from "react";
 import Header from "@/app/components/Header";
 
 export default function Home() {
+  useEffect(() => {
+    return () => {
+      darkModeSwitcher();
+    };
+  }, []);
+
+  const darkModeSwitcher = () => {
+    if (
+      localStorage.getItem("color-theme") === "dark" ||
+      (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
   return (
     <>
       <Header />
