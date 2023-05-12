3bab870534a401300424eba53d7a78d1f1fc2971
kenvilar <kenvilar@gmail.com>
2023-05-12 14:25:31 +0800

    minor fix

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -92,7 +92,7 @@ const Header = () => {
             {/*Light SVG Icon*/}
             <svg
               id="theme-toggle-light-icon"
-              className="w-5 h-5"
+              className="w-5 h-5 hidden"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
